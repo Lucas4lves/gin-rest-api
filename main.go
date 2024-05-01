299ea89b142d1a367b1d57411912856f0572310a
lucas4lves <zombnfl@gmail.com>
2024-05-01 16:49:15 -0300

    Feat: custom headers

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-//type with json annotations
+// type with json annotations
 type Album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
@@ -13,17 +14,17 @@ type Album struct {
 	Price  float64 `json:"price"`
 }
 
-//Mocked up data
+// Mocked up data
 var albums = []Album{
-	{ID: "1", Title: "Rush of Blood To the Head", Artist:"Coldplay", Price: 17.99 },
-	{ID: "2", Title: "X and Y", Artist:"Coldplay", Price: 45.89 },
-	{ID: "3", Title: "Viva La Vida", Artist: "Coldplay", Price: 27.99 },
+	{ID: "1", Title: "Rush of Blood To the Head", Artist: "Coldplay", Price: 17.99},
+	{ID: "2", Title: "X and Y", Artist: "Coldplay", Price: 45.89},
+	{ID: "3", Title: "Viva La Vida", Artist: "Coldplay", Price: 27.99},
 }
 
-func PostAlbum(c *gin.Context){
+func PostAlbum(c *gin.Context) {
 	var newAlbum Album
 
-	if err := c.BindJSON(&newAlbum); err != nil{
+	if err := c.BindJSON(&newAlbum); err != nil {
 		return
 	}
 
@@ -31,20 +32,21 @@ func PostAlbum(c *gin.Context){
 	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
-func GetAlbumById(c *gin.Context){
-	id := c.Param("id")	
-	for _, a := range albums{
-		if a.ID == id{
+func GetAlbumById(c *gin.Context) {
+	id := c.Param("id")
+	for _, a := range albums {
+		if a.ID == id {
 			c.IndentedJSON(http.StatusOK, a)
-			return 
+			return
 		}
 	}
 
 	c.IndentedJSON(http.StatusNotFound, gin.H{"msg": "album not found"})
 }
 
-func GetAllAlbums(c *gin.Context){
-	c.IndentedJSON(http.StatusOK, albums)	
+func GetAllAlbums(c *gin.Context) {
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	c.JSON(http.StatusOK, albums)
 }
 
 func main() {
